82f266d8b853b9fb6f513b6d0e69d3a7998e0afd
henry torres <henrytorrescl@gmail.com>
2023-10-06 02:02:38 -0400

    Final product

--- pages/steam.py
+++ pages/steam.py
@@ -1,21 +1,11 @@
-import pandas as pd
 import streamlit as st
-import numpy as np
-import re
-import nltk
-from nltk.corpus import stopwords
-from wordcloud import WordCloud
+import pandas as pd
 import matplotlib.pyplot as plt
-import random as rn
 import seaborn as sns
-from plotly import graph_objs as go
-import plotly.express as px
-import plotly.figure_factory as ff
-from collections import Counter
-from PIL import Image
+from wordcloud import WordCloud
+from streamlit_elements import nivo
 from streamlit_elements import elements, mui, html
-pd.options.display.max_rows = None
-#import opendatasets as od
+import plotly.express as px
 
 
 st.set_page_config(layout="wide")
@@ -39,51 +29,55 @@ sample_percentage = 20  # Change this to your desired percentage
 # Loading the data
 fp1 = './data/dataset.csv'
 df1 = load_steam_data(fp1, sample_percentage)
-df1 = df1.drop(df1[df1.review_text == 'Early Access Review'].index)
 
 fp2 = './data/steam.csv'
 df2 = load_steam_data(fp2)
 
 split_genres = df2["genres"].str.split(";", n=1, expand=True)
 df2["genres"] = split_genres[0]
-game_genres = df2["genres"].value_counts()[:15]
+game_genres = df2["genres"].value_counts()[:10]
 
 selected_genre = st.sidebar.selectbox("Select Genre:", game_genres.index)
 
-col1, col2 = st.columns( [4,4] )
+col1, col2= st.columns( [4,4] )
 
 with col1:
-    st.write('Genre Graph')
-    # create a list of all the state names
-    game_genres.plot(kind="barh", color='#005f8b')
-    plt.gca().invert_yaxis()
+    st.write('### Genre Distribution')
+    if selected_genre:
+        # Prepare data for Plotly Bar Chart
+        pie_data = game_genres.reset_index()
+        pie_data.columns = ['Category', 'Count']
 
-    plt.xlabel('Amount of Games in Category')
-    plt.ylabel('Category')
-    plt.title('Top 15 Categoires with the Most Games')
-
-    sns.set(rc={'figure.figsize':(10,8)})
-
-    st.pyplot(plt.gcf())
+        # Create Plotly Bar Chart
+        fig = px.bar(
+            pie_data,
+            x="Category",
+            y="Count",
+            labels={"Category": "Genre", "Count": "Number of games"},
+            title="Genre Distribution",
+        )
+        st.plotly_chart(fig)
 
 
 with col2:
     st.write(f'{selected_genre} Genre Descriptions')
     combined_df = pd.merge(df1, df2, left_on='app_id', right_on='appid', how='left')
     combined_df.review_text = combined_df.review_text.astype('str')
+    combined_df['review_text'] = combined_df['review_text'].str.replace('Early Access Review', '', case=False)
     genre_df = combined_df[combined_df['genres'] == selected_genre]
     txt = ' '.join(rev for rev in genre_df.review_text)
-    plt.figure(figsize=(15,8))
-
+    
+    plt.figure(figsize=(15, 8))
     wordcloud = WordCloud(
-            background_color = 'black',
-            max_font_size = 100,
-            max_words = 100,
-            width = 1000,
-            height = 600
-            ).generate(txt)
-
-
-    plt.imshow(wordcloud,interpolation = 'bilinear')
+        background_color='black',
+        max_font_size=100,
+        max_words=100,
+        width=1000,
+        height=600
+    ).generate(txt)
+    plt.imshow(wordcloud, interpolation='bilinear')
     plt.axis('off')
     st.pyplot(plt.gcf())
+
+
+
